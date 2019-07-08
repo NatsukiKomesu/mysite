b206aefb5dacf524a8a9740e2492ee7e09a2ad8e
NatsukiKomesu <ricevinegar0424@outlook.jp>
2019-07-08 21:11:19 +0900

    first commit

---
+++ music.hyml_files/ajs(1).php
@@ -0,0 +1,9 @@
+﻿(function(){
+var s = "<"+"script type=\"text/javascript\">window.bannerStartTimestamp = Date.now();<"+"/script><"+"div id=\'beacon_5d22fa24a10af59ad693c6b9279dbe46\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
+s += "<"+"img width=\"0\" height=\"0\" src=\"https://cat.jp.as.criteo.com/delivery/lg.php?cppv=3&cpp=xcOdCA---1sxevSkLrh202NGLC8E2D96aA9tTzyLbd_T8ru_Bw_vekktO0PW3kzlzxwuX2Xiz0_ndITYS_5x18vLfFN95Y9agzZHGb2CKulFg_GRfpXihmKkb1OcQYBrxw3K5MTA9JwGgPTstK3PBnl93Y6k8QTVa-a10NIH-fKWaT_Ff85KmNvIaOfReTJF1R1UdMwqqBCAo3atr_XEXpbCNqxiEqSv9lWBY_vjZaS49-67PAKyOtdA7-0LQ2zu_ISzHw\"/>\n";
+s += "<"+"/div>\n";
+s += "<"+"script type=\'text/javascript\'>\n";
+s += "gCRITEO.passback();\n";
+s += "<"+"/script>\n";
+s += "\n";
+document.write(s);})();
---
+++ music_files/ajs.php
@@ -0,0 +1,9 @@
+﻿(function(){
+var s = "<"+"script type=\"text/javascript\">window.bannerStartTimestamp = Date.now();<"+"/script><"+"div id=\'beacon_5d22fa259a3d64b150a8afecaa44ff53\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
+s += "<"+"img width=\"0\" height=\"0\" src=\"https://cat.jp.as.criteo.com/delivery/lg.php?cppv=3&cpp=rE_n4qotvB1-2aLxiZp5XCANdGJ3t63U9eGzKQnd2cwHgry8E2R1SUZiW0DFk_je0kq-U0_nCORFz-byF1p9TzuOhCFQzgZFsD0PcyDxStgCWTiKQxeU-nnt4670qjY1cWZtBKj57sYjcPSEP65N_7UB04MYdIVyZ9CeDXDhaEx1iCwyNIodiwfpsCJw4fBr8NN4xrdc-KXp4on035oKY4lBuEurDhcvQny1LXHkDuPijn847jlDiDVZdCwdYuPzcNSaGA\"/>\n";
+s += "<"+"/div>\n";
+s += "<"+"script type=\'text/javascript\'>\n";
+s += "gCRITEO.passback();\n";
+s += "<"+"/script>\n";
+s += "\n";
+document.write(s);})();
